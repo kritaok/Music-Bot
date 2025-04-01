b953507eef12f5cedf37e47aaa1ffc53c30cbae7
Kritanon Trachookul <98570658+kritaok@users.noreply.github.com>
2025-04-01 16:12:44 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,27 @@
+
+
+module.exports = {
+  TOKEN: "MTM1NjUzNTY1NDgyOTI2MDg2OQ.GdCEg2.JQkZt2QIWfuUAOYnNcRWQkvnOMSg_hbjtOvrXA",
+  language: "en",
+  ownerID: ["918334654816329750", ""], 
+  mongodbUri : "mongodb+srv://SiangStudio:q5qcKchKi4Ft2phw@cluster0.gfw9a38.mongodb.net/?retryWrites=true&w=majority",
+  spotifyClientId : "",
+  spotifyClientSecret : "",
+  setupFilePath: './commands/setup.json',
+  commandsDir: './commands',  
+  embedColor: "#1db954",
+  activityName: "YouTube Music", 
+  activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
+  SupportServer: "https://discord.gg/KvEJs8zAMh",
+  embedTimeout: 5, 
+  errorLog: "", 
+  nodes: [
+     {
+      name: "GlaceYT",
+      password: "glaceyt",
+      host: "193.226.78.187",
+      port:  9372,
+      secure: false
+    }
+  ]
+}
