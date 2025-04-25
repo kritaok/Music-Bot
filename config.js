0e4d60a8489d6920038ef5710e76e5443e3a40a0
Kritanon Trachookul <98570658+kritaok@users.noreply.github.com>
2025-04-25 10:59:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -17,11 +17,11 @@ module.exports = {
   errorLog: "", 
   nodes: [
      {
-      name: "GlaceYT",
-      password: "glaceyt",
-      host: "193.226.78.187",
-      port:  9372,
-      secure: false
+      name: "DSC.gg",
+      password: "https://dsc.gg/ajidevserver",
+      host: "lava-v3.ajieblogs.eu.org",
+      port:  443,
+      secure: true
     }
   ]
 }
